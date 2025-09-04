e84adbb14a45420eeb5b4e9236e85f04dcd4a578
claudirdev <claudir.dev@gmail.com.br>
2025-09-04 15:30:12 -0300

    Atividade 5

--- Atividade_5/react-omdb-app/src/config.js
+++ Atividade_5/react-omdb-app/src/config.js
@@ -4,5 +4,5 @@ Subject: Exercicio 5 - Consumo API com ReactJS - Formação +PraTI
 Create: 21/08/07/25                                                   
 Developer: Claudir Kraulich
 -------------------------------------------------------------------*/
-export const API_KEY = "8b3211c6";
+export const API_KEY = "sua chave aqui";
 export const BASE_URL = "https://www.omdbapi.com/";
